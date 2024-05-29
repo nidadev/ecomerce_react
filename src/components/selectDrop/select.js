89fb3d2096d4130e18f6428b86b1d113201966b6
nida <nidazeeshan15@gmail.com>
2024-05-29 21:26:45 +0500

    upd

--- src/components/selectDrop/select.js
+++ src/components/selectDrop/select.js
@@ -17,18 +17,18 @@ const Select = ()=>
                 <div className="SearchField">
                     <input type="text"></input>
                     <ul className="searchResults">
-                        <li>Milks and Dairies</li>
-                        <li>Wines & Drinks</li>
-                        <li>Clothing & beauty</li>
-                        <li>Fresh Seafood</li>
-                        <li>Pet Foods & Toy</li>
-                        <li>Fast food</li>
-                        <li>Baking material</li>
-                        <li>Vegetables</li>
+                        <li onClick={()=>setisOpenSelect(false)}>Milks and Dairies</li>
+                        <li onClick={()=>setisOpenSelect(false)}>Wines & Drinks</li>
+                        <li onClick={()=>setisOpenSelect(false)}>Clothing & beauty</li>
+                        <li onClick={()=>setisOpenSelect(false)}>Fresh Seafood</li>
+                        <li onClick={()=>setisOpenSelect(false)}>Pet Foods & Toy</li>
+                        <li onClick={()=>setisOpenSelect(false)}>Fast food</li>
+                        <li onClick={()=>setisOpenSelect(false)}> Baking material</li>
+                        <li onClick={()=>setisOpenSelect(false)}>Vegetables</li>
 
-                        <li>Fresh Fruit</li>
-                        <li>Bread and Juice</li>
-                        <li>Pet Foods & Toy</li>
+                        <li onClick={()=>setisOpenSelect(false)}>Fresh Fruit</li>
+                        <li onClick={()=>setisOpenSelect(false)}>Bread and Juice</li>
+                        <li onClick={()=>setisOpenSelect(false)}>Pet Foods & Toy</li>
                     </ul>
                 </div>
             </div>
