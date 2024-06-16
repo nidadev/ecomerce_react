10b15b24760df002824b5f0134ab2b62e4f7441b
nida <nidazeeshan15@gmail.com>
2024-06-16 14:14:09 +0500

    upd

--- src/components/selectDrop/select.js
+++ src/components/selectDrop/select.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import '../selectDrop/select.css'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { ClickAwayListener } from '@mui/base/ClickAwayListener';
+import { List } from "@mui/icons-material";
 
 
 
@@ -10,6 +11,10 @@ const Select = ({data,placeholder,icon})=>
         const [isOpenSelect, setisOpenSelect] = useState(false)
                 const [SelectedIndex, setSelectedIndex] = useState(0)
                 const [SelectedItem, setSelectedItem] = useState(placeholder)
+
+                const [Listdata,setListdata] = useState(data)
+                const [Listdata2,setListdata2] = useState(data)
+
 
 
         const openSelect =()=>{
@@ -21,6 +26,20 @@ const Select = ({data,placeholder,icon})=>
             setisOpenSelect(false);
             setSelectedItem(name);
         }
+        const filterList=(e)=>
+        {
+            const keyword = e.target.value.toLowerCase();
+            //console.log(keyword);
+            const list = Listdata2.filter((item)=>
+                {
+                    return item.toLowerCase().includes(keyword);
+                });
+                const list2 = list.filter((item,index) => 
+                    list.indexOf(item) === index);
+                setListdata(list2);
+                //console.log(List);
+
+        }
         return(
             <ClickAwayListener onClickAway={()=> {setisOpenSelect(false)}}>
                  <div className='selectDropWrapper cursor position-relative'>
@@ -30,12 +49,12 @@ const Select = ({data,placeholder,icon})=>
            isOpenSelect === true &&
             <div className="selectDrop">
                 <div className="SearchField">
-                    <input type="text" placeholder="Search here..."></input>
+                    <input type="text" placeholder="Search here..." onChange={filterList}></input>
                     <ul className="searchResults">
                     <li key={0} onClick={()=>closeSelect(0,placeholder)} className={`${setSelectedIndex===0 ?'active':''}`}>{placeholder}</li>
 
                         {
-                        data.map((item,index) => {
+                        Listdata.map((item,index) => {
                             return(
                                 <li key={index+1} onClick={()=>closeSelect(index+1,item)} className={`${setSelectedIndex===index+1 ?'active':''}`}>{item}</li>
 
