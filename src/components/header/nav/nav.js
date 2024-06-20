60452e10abd54582e7c693ce8970016816d611c1
nida <nidazeeshan15@gmail.com>
2024-06-20 23:43:21 +0500

    upd

---
+++ src/components/header/nav/nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = ()=> {
+    return(
+        <div className="nav">
+            <div className="container-fluid">
+                
+            </div>
+            </div>
+    )
+
+    }
+
+export default Nav; 
