6b85ab726c2f5746cdb265ab483796d00b0f7b62
nida <nidazeeshan15@gmail.com>
2024-06-21 23:22:37 +0500

    upd

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/header/header.js';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/index';
+import About from './pages/About/index';
+
 
 function App() {
   return (
     <BrowserRouter>
           <Header />
-          <Routes>
-            <Route exact={true} path="/" element={}></Route>
-          </Routes>
-          </BrowserRouter>
-        
+
+     <Routes>
+         <Route path="/react" element={<Home />} />
+         <Route path="/react/about" element={<About />} />
+
+     </Routes>
+</BrowserRouter>
   );
 }
 
 export default App;
+
--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -3,6 +3,8 @@ import './nav.css';
 import Button from '@mui/material/Button';
 import CategoryOutlinedIcon from '@mui/icons-material/CategoryOutlined';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { Link } from "react-router-dom";
+import HeadphonesOutlinedIcon from '@mui/icons-material/HeadphonesOutlined';
 
 
 
@@ -11,27 +13,32 @@ const Nav = ()=> {
         <div className="nav d-flex align-items-center">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-sm-3">
+                    <div className="col-sm-3 d-flex align-items-center">
                     <Button className="bg-success text-white catTab"><CategoryOutlinedIcon />&nbsp;Browse All Categories<KeyboardArrowDownIcon /></Button>
 
                     </div>
                     <div className="col-sm-7">
                         <nav>
-                            <ul className="list list-inline">
-                                <li className="list-inline-item">Home</li>
-                                <li>About</li>
-                                <li>Shop</li>
-                                <li>Vendors</li>
-                                <li>Mega menu</li>
-                                <li>Blog</li>
-                                <li>Pages</li>
-                                <li>Contact</li>
+                            <ul className="list list-inline mb-0">
+                                <li className="list-inline-item"><Button><Link>Home</Link></Button></li>
+                                <li className="list-inline-item"><Button><Link>About</Link></Button></li>
+                                <li className="list-inline-item"><Button><Link>Shop<KeyboardArrowDownIcon /></Link></Button></li>
+                                <li className="list-inline-item"><Button><Link>Vendors</Link></Button></li>
+                                <li className="list-inline-item"><Button><Link>Mega menu<KeyboardArrowDownIcon /></Link></Button></li>
+                                <li className="list-inline-item"><Button><Link>Blog</Link></Button></li>
+                                <li className="list-inline-item"><Button><Link>Pages<KeyboardArrowDownIcon /></Link></Button></li>
+                                <li className="list-inline-item"><Button><Link>Contact</Link></Button></li>
 
                             </ul>
                         </nav>
 
                     </div>
-                    <div className="col-sm-2">
+                    <div className="col-sm-2 d-flex align-items-center">
+                        <div className="phNo d-flex align-items-center">
+                        <span><HeadphonesOutlinedIcon /></span>
+                        <div className="info ml-3">
+                            <h4 className="text-g">1900 - 888</h4></div>
+                        </div>
 
                     </div>
                 </div>
